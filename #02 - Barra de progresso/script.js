c06df7cfdeec7ac967557f96b7f2265554739168
vaansuit <vvansuit@outlook.com>
2022-06-01 15:41:39 -0300

    commit

---
+++ #02 - Barra de progresso/script.js
@@ -0,0 +1,54 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle') //usado selector all pois possuí diversos circulos
+
+let currentActive = 1;
+
+next.addEventListener('click', () => {
+    currentActive++
+
+    if (currentActive > circles.length) {
+        currentActive = circles.length
+    }
+
+    update()
+    // console.log(currentActive)
+})
+
+prev.addEventListener('click', () => {
+    currentActive--
+
+    if(currentActive < 1) {
+        currentActive =1
+    }
+
+    update()
+    //console.log(currentActive)
+})
+
+//criando a função update, que vai atualizar os circulos dos steps
+
+function update() {
+    circles.forEach((circle, idx) => {
+        if (idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+    if (currentActive === 1) {
+        prev.disabled = true
+    } else if (currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+}
+
