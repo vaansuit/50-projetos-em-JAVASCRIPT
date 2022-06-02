0e1ef821531a056450e97821013570c0e5c7ccc9
vaansuit <vvansuit@outlook.com>
2022-06-02 19:07:54 -0300

    KeyCode Clone

---
+++ #10 - Dad Jokes - Integração com API de terceiros/script.js
@@ -0,0 +1,22 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+generateJoke()
+
+jokeBtn.addEventListener('click', generateJoke)
+
+function generateJoke() {
+    const config = {
+        headers: {
+            Accept : 'application/json',
+        }
+    }
+
+    fetch('https://icanhazdadjoke.com/', config)
+    .then((res) => res.json())
+    .then((data) => /*console.log(data)*/{
+        jokeEl.innerHTML = data.joke
+    })
+
+
+}
