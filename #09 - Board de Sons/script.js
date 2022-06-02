01f1538c9e1c7254bafc43059b7e87aadd4049c3
vaansuit <vvansuit@outlook.com>
2022-06-02 17:05:03 -0300

    Board de Sons

---
+++ #09 - Board de Sons/script.js
@@ -0,0 +1,24 @@
+const sounds = ['som1', 'som2', 'som3', 'som4'];
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
